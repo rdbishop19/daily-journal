a84692d952291a6e09119284998f3ea32553dbc1
Ryan Bishop <rdbishop06@gmail.com>
2019-10-14 16:09:14 -0500

    Added initial 3-object array

---
+++ src/scripts/journal.js
@@ -0,0 +1,24 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntries = [
+    {
+        date: "09/30/2019",
+        concept: "Basic HTML/CSS",
+        entry: "We learned about basic HTML/CSS setup for a typical application. This was mostly review from what I learned in JumpStart and pre-work.",
+        mood: "Motivated"
+    },
+    {
+        date: "10/01/2019",
+        concept: "CSS Selectors",
+        entry: "We learned about styling a webpage using css and keeping the html source code linked to the css styles with ids and classes.",
+        mood: "Excited"
+    },
+    {
+        date: "10/02/2019",
+        concept: "DOM Manipulation",
+        entry: "We learned about making changes to a webpage using Javascript DOM methods.",
+        mood: "Tired"
+    }
+]
