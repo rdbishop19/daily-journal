5c90f3b23957186a79ef7d22851eaeab675b6c38
Ryan Bishop <rdbishop06@gmail.com>
2019-10-15 16:00:41 -0500

    Setup json-server and add fetch call to print journal entries to DOM

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,50 +1,16 @@
-/*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned today
-*/
-const journalEntries = [
-    {
-        date: "09/30/2019",
-        concept: "Basic HTML/CSS",
-        entry: "We learned about basic HTML/CSS setup for a typical application. This was mostly review from what I learned in JumpStart and pre-work.",
-        mood: "Motivated"
-    },
-    {
-        date: "10/01/2019",
-        concept: "CSS Selectors",
-        entry: "We learned about styling a webpage using css and keeping the html source code linked to the css styles with ids and classes.",
-        mood: "Excited"
-    },
-    {
-        date: "10/02/2019",
-        concept: "DOM Manipulation",
-        entry: "We learned about making changes to a webpage using Javascript DOM methods.",
-        mood: "Tired"
-    }
-]
 
-/*
-    Purpose: To create, and return, a string template that
-    represents a single journal entry object as HTML
-
-    Arguments: journalEntry (object)
-*/
 const makeJournalEntryComponent = (journalEntry) => {
     // Create your own HTML structure for a journal entry
     return `
         <section class="entry">
-            <h2>${journalEntry.concept}</h2>
+        <h2>${journalEntry.concept}</h2>
             <h3>${journalEntry.date}</h3>
             <p>${journalEntry.entry}</p>
             <p>Mood: ${journalEntry.mood}</p>
         </section>
-    `
+        `
 }
 
-/*
-    Purpose: To render all journal entries to the DOM
-    Arguments: entries (array of objects)
-*/
 let entryList = "";
 const renderJournalEntries = (entries) => {
     entries.forEach((entry) => {
@@ -54,4 +20,11 @@ const renderJournalEntries = (entries) => {
 }
 
 // Invoke the render function
-renderJournalEntries(journalEntries)
+// renderJournalEntries(journalEntries)
+
+fetch("http://localhost:3000/entries") // Fetch from the API
+.then(entries => entries.json())  // Parse as JSON
+.then(parsedEntries => {
+    renderJournalEntries(parsedEntries)
+        // What should happen when we finally have the array?
+    })
