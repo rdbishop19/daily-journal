88c1a84e50aedfc7c8eee827979caee949da8c69
Ryan Bishop <rdbishop06@gmail.com>
2019-10-17 19:55:50 -0500

    refactor: functions into separate components

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,11 +1,3 @@
-// ~ ~ previous code ~ ~
-// fetch('http://localhost:3000/entries') // Fetch from the API
-// 	.then((entries) => entries.json()) // Parse as JSON
-// 	.then((parsedEntries) => {
-// 		renderJournalEntries(parsedEntries);
-// 		// What should happen when we finally have the array?
-// 	});
-
 const API = {
 	getJournalEntries() {
 		return fetch('http://localhost:3000/entries').then((response) => response.json());
