49bee4ef378f349f704fd931150bc4f122fffb61
Ryan Bishop <rdbishop06@gmail.com>
2019-10-20 17:20:09 -0500

    feat: submit button event listener + newEntryObject factory function

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -8,3 +8,24 @@ import dom from "./dom.js"
 data.getJournalEntries().then(entries => { 
     dom.renderJournal(entries);
 })
+
+const createEntryObject = (date, concepts, description, mood) => {
+    return {
+        date: date.value,
+        concept: concepts.value,
+        description: description.value,
+        mood: mood.value
+    }
+}
+
+const submitButtonHandler = () => {
+    let journalDate = document.querySelector("#journalDate")
+    let conceptsCovered = document.querySelector("#conceptsCovered")
+    let journalEntry = document.querySelector("#journalEntry")
+    let mood = document.querySelector("#mood")
+
+    const newJournalEntry = createEntryObject(journalDate, conceptsCovered, journalEntry, mood)
+    console.log(newJournalEntry);
+}
+
+document.querySelector("button").addEventListener("click", submitButtonHandler);
