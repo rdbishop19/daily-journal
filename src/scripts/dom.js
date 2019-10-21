ea791036d40767338085937798ada3413fd1dde2
Ryan Bishop <rdbishop06@gmail.com>
2019-10-20 20:34:59 -0500

    feat: add character count + max length to concepts covered form field

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -23,8 +23,33 @@ const dom = {
                 .then(data.getJournalEntries) // don't invoke function = ex. getJournalEntries()
                 .then(entries => dom.renderJournal(entries))
         }
-        
         document.querySelector("button").addEventListener("click", getFormFields);
+    },
+    addCharacterCountEvent() {
+        const characterEventHandler = () => {
+            let characterCount = document.querySelector("#conceptsCovered").value.length
+            let maxCharNotice = document.querySelector("#characters-remaining")
+            let conceptsCovered = document.getElementById("conceptsCovered").value;
+            let maxCharLength = 80;
+            let displayAtNumber = maxCharLength - 15;
+            
+            console.log(characterCount);
+            if (characterCount <= displayAtNumber){
+                maxCharNotice.textContent = "";
+                maxCharNotice.style.color = "black";               
+            }
+            if (characterCount < maxCharLength){
+                maxCharNotice.classList.remove("warning");
+                maxCharNotice.textContent = `${maxCharLength - characterCount} remaining`
+            }
+            else if (characterCount = maxCharLength ){
+                // maxCharNotice.textContent = "*Too many characters"
+                maxCharNotice.textContent = "0 remaining";
+                maxCharNotice.classList.add("warning");
+                document.querySelector("#conceptsCovered").value = conceptsCovered.slice(0, maxCharLength);
+            }
+        }
+        document.querySelector("#conceptsCovered").addEventListener("keyup", characterEventHandler)
     }
 }
 
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -3,8 +3,8 @@
     defined in the other JavaScript files.
 */
 import data from "./data.js"
-import dom from "./dom.js"
-import entry from "./entry.js";
+import dom from "./dom.js";
 
 data.getJournalEntries().then(entries => dom.renderJournal(entries));
 dom.createSubmitHandler();
+dom.addCharacterCountEvent();
